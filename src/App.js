f4c1c4b02fabc6096cf2179e89f83038ba50f198
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-11 21:37:06 +0100

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Tweet from './Tweet'
 
-function App() {
+const App = () => {
+/* const say = () => {
+  console.log('Hello world');
+};
+const sayText = 'Say'; */
+  /* const [isRed,setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  } */
+  
+  const [users, setUsers] = useState([
+    { name:'Mike', message:'Hello there!!' },
+    { name:'James', message:'Hi!' },
+    { name:'Ayo', message:'Hey!'}
+  ]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
 
-export default App;
+export default App
+
+/* <Tweet name='mike olud' message='Hello world'/>
+      <Tweet name='mike oluda' message='Hello aliens' />
+      <Tweet name='mike oludar' message='Hello peeps' />
+      <Tweet name='mike oludare' message='Hello bitches' /> */
+
+      /* <h1 className={isRed ? 'red': ''}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1> */
